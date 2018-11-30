2495512241c8e9218cf8c28859d822943db07a5f
ZEROssk <akinori_17N1101034@nnn.ed.jp>
2018-11-30 20:26:22 +0900

    コメントアウト追加

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import youtube_dl
 
 url = input('> Prease enter URL : ')
 options = {
-  'format': 'bestaudio[ext=mp3]/bestaudio[ext=mp3]/bestaudio'
+  'format': 'bestaudio[ext=mp3]/bestaudio[ext=mp3]i/bestaudio'
 }
 
 with youtube_dl.YoutubeDL(options) as ydl:
     ydl.download([url])
+
+#youtube-dl -saf mp4 list.text
 
