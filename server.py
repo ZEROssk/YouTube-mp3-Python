60b3c3dadb66aca660480f64ec7df06c8d128240
ZEROssk <a.k.jack.ssk@gmail.com>
2019-11-25 13:15:53 +0900

    update server.go

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 # coding:utf-8
 from flask import Flask, jsonify, make_response, request, Response
 import json
+import youtube_dl
 
 app = Flask(__name__)
+
+def downloadMedia(url, op):
+    with youtube_dl.YoutubeDL(op) as ydl:
+        ydl.download([url])
 
 def responseJSON(url):
     status = 0
@@ -20,28 +25,30 @@ def responseJSON(url):
 
 @app.route('/md/api/mp3', methods=['POST'])
 def postJsonMp3():
+    options = {
+            'format': 'bestaudio[ext=mp3]/bestaudio'
+            }
+
     json = request.get_json()
     URL = json['url']
+
+    downloadMedia(URL, options)
     return jsonify(responseJSON(URL))
 
 @app.route('/md/api/mp4', methods=['POST'])
 def postJsonMp4():
+    options = {
+            'format': 'bestaudio[ext=mp3]/bestaudio'
+            }
+
     json = request.get_json()
     URL = json['url']
+
+    downloadMedia(URL, options)
     return jsonify(responseJSON(URL))
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=3600)
 
-# import youtube_dl
-#
-# url = input('>Prease enter URL : ')
-# options = {
-#   'format': 'bestaudio[ext=mp3]/bestaudio[ext=mp3]i/bestaudio'
-# }
-#
-# with youtube_dl.YoutubeDL(options) as ydl:
-#     ydl.download([url])
-#
-# #youtube-dl -saf mp4 list.text
+#youtube-dl -saf mp4 list.text
 
