75f47d38bdc444c918a4529e6375558ddc361583
zerossk <a.k.jack.ssk@gmail.com>
2020-02-18 00:41:12 +0900

    Add UserScript file

---
+++ youtube_mp3_download.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         YouTube MP3 Downloader
+// @namespace    http://zerono.teamfruit.net
+// @version      0.1
+// @description  MP3 Download to server from YouTube
+// @author       ZEROssk
+// @match        https://www.youtube.com/watch?v=RoKLtsR6K4k
+// @grant        none
+// @license      MIT License
+// @updateURL    
+// ==/UserScript==
+
+(function () {
+    'use strict';
+})();
