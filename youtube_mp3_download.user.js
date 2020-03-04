f23d8a006bbc8a7c7daab82afbbcdbb3f2c03acf
ZEROssk <a.k.jack.ssk@gmail.com>
2020-03-04 13:20:29 +0900

    user script test

--- youtube_mp3_download.user.js
+++ youtube_mp3_download.user.js
@@ -4,24 +4,26 @@
 // @version      0.1
 // @description  MP3 Download to server from YouTube
 // @author       ZEROssk
-// @match        https://www.youtube.com/watch*
+// @include      https://www.youtube.com*
 // @grant        none
 // @license      MIT License
 // @updateURL    https://github.com/ZEROssk/mp3-download-to-server-from-youtube/raw/master/youtube_mp3_download.user.js
 // ==/UserScript==
 
 (function () {
-	'use strict';
-
-	window.onload = function() {
-		let btCreate = document.getElementsByTagName("ytd-subscribe-button-renderer")[0]
-		let button =
-			'<button id="api-button">Button</button>'
-		;
-		btCreate.insertAdjacentHTML('beforebegin', button);
-
-		console.log("done");
-	}
-
+    'use strict';
+    window.onload = function() {
+        console.log("done");
+        function click() {
+            console.log("click");
+        }
+        let findArea = document.getElementsByClassName("style-scope ytd-menu-renderer")[0]
+        let btn =
+            "<div>"+
+                "<button onclick='click()'>Click</button>"+
+            "</div>"
+        ;
+        findArea.insertAdjacentHTML('afterbegin', btn);
+        console.log(findArea);
+    }
 })();
-
